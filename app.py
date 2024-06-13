a6173491d3425fda6a9159d11ce04a54b92b89f0
delprincip3 <delprincipeluigimichele@gmail.com>
2024-06-13 20:28:33 +0200

    inizializzazione flask e db

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request, flash, redirect, url_for, session,jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+from datetime import timedelta
+from flask_migrate import Migrate
+
+
+
+
+
+app = Flask(__name__, template_folder='src', static_folder='src')
+app.permanent_session_lifetime = timedelta(minutes=5)
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:Luigi2005@localhost:3306/gestioneturni_boschetto"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'supersecret'
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+class Utenza(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    tipo = db.Column(db.String(50), nullable=False)
+    nome = db.Column(db.String(100), nullable=False)
+    cognome = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+  
+
+    def __init__(self, tipo, nome, cognome, email, password):
+        self.tipo = tipo
+        self.nome = nome
+        self.cognome = cognome
+        self.email = email
+        self.password = password
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
---
+++ forms.py
@@ -0,0 +1,19 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, SelectField, DateField, TextAreaField
+from wtforms.validators import DataRequired, Email, EqualTo,Optional
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+class EliminaUtenteForm(FlaskForm):
+    submit = SubmitField('Elimina')
+
+class ModificaUtenteForm(FlaskForm):
+    tipo = SelectField('Tipo', choices=[('Admin', 'Admin'), ('User', 'User')], validators=[DataRequired()])
+    nome = StringField('Nome', validators=[DataRequired()])
+    cognome = StringField('Cognome', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Salva')
