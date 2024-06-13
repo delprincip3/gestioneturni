d25cb2121826a275a00ef4c9460213611d469bd1
delprincip3 <delprincipeluigimichele@gmail.com>
2024-06-13 21:40:54 +0200

    prima parte

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
-from flask import Flask, render_template, request, flash, redirect, url_for, session,jsonify
+from flask import Flask, render_template, request, flash, redirect, url_for, session, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from forms import LoginForm,EliminaUtenteForm,ModificaUtenteForm
-from datetime import timedelta
 from flask_migrate import Migrate
-
-
-
-
+from datetime import timedelta
+from forms import LoginForm, RegisterForm, EliminaUtenteForm, ModificaUtenteForm
 
 app = Flask(__name__, template_folder='src', static_folder='src')
 app.permanent_session_lifetime = timedelta(minutes=5)
@@ -17,7 +13,6 @@ app.config['SECRET_KEY'] = 'supersecret'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-
 class Utenza(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     tipo = db.Column(db.String(50), nullable=False)
@@ -26,7 +21,6 @@ class Utenza(db.Model):
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
   
-
     def __init__(self, tipo, nome, cognome, email, password):
         self.tipo = tipo
         self.nome = nome
@@ -38,7 +32,35 @@ class Utenza(db.Model):
 def index():
     return render_template('index.html')
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
+        utente = Utenza.query.filter_by(email=email).first()
+
+        if utente and utente.password == password:
+            session['user_id'] = utente.id
+            session.permanent = True  # per mantenere la sessione permanente per la durata specificata
+            flash('Login eseguito con successo!', 'success')
+
+            if utente.tipo == 'admin':
+                return redirect(url_for('dashboard_admin'))
+            else:
+                return redirect(url_for('dashboard_user'))
+        else:
+            flash('Email o password non validi. Riprova.', 'danger')
+
+    return render_template('login.html', form=form)
+
+@app.route('/dashboardadmin')
+def dashboard_admin():
+    return render_template('dashboard_admin.html')
+
+@app.route('/dashboarduser')
+def dashboard_user():
+    return render_template('dashboard_user.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
-
--- forms.py
+++ forms.py
@@ -2,7 +2,14 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, SelectField, DateField, TextAreaField
 from wtforms.validators import DataRequired, Email, EqualTo,Optional
 
-
+class RegisterForm(FlaskForm):
+    tipo = SelectField('Tipo', choices=[ ('User', 'User')], validators=[DataRequired()])
+    nome = StringField('Nome', validators=[DataRequired()])
+    cognome = StringField('Cognome', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+   
+    submit = SubmitField('Registrati')
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
